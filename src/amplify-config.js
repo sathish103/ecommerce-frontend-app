2e3d2c7ac191f29de62ac8419774a7ead11ea7a3
sathish103 <sathishreddys786@gmail.com>
2025-06-15 16:15:41 +0530

    ecommerce frontend code

--- src/amplify-config.js
+++ src/amplify-config.js
@@ -3,11 +3,9 @@ import { Amplify } from 'aws-amplify';
 
 Amplify.configure({
   Auth: {
-    Cognito: {
-      region: process.env.REACT_APP_COGNITO_REGION,
-      userPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
-      userPoolClientId: process.env.REACT_APP_COGNITO_APP_CLIENT_ID,
-      // Only if you're NOT using OAuth, you can omit loginWith here
-    }
+    region: process.env.REACT_APP_COGNITO_REGION,
+    userPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
+    userPoolWebClientId: process.env.REACT_APP_COGNITO_APP_CLIENT_ID,
+    authenticationFlowType: 'USER_PASSWORD_AUTH'  // optional, but helps with username-password login
   }
 });
