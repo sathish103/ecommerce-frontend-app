36d1cc8c85807247bc890537b8c597b37e3cd77a
sathish103 <sathishreddys786@gmail.com>
2025-06-18 03:26:33 +0530

    updated code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Optional: if you have global styles
+//import "./index.css"; // Optional: if you have global styles
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
