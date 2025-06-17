3a28348b50e4d6bb5f00db149655929705ddccb9
sathish103 <sathishreddys786@gmail.com>
2025-06-18 03:44:08 +0530

    updated code

--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import authService from "../../services/authService";
+import authService from "../../services/adminService";
 import { useNavigate } from "react-router-dom";
 
 const RegisterForm = () => {
